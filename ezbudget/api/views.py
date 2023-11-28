35c6d2905c03591ec58d43290cb6ace2099abb67
Jonathan Davidson <jdavidson@netsplasher.com>
2023-11-28 07:22:04 -0600

    Add accounts route

--- ezbudget/api/views.py
+++ ezbudget/api/views.py
@@ -1,7 +1,21 @@
-from flask import Blueprint
+from flask import Blueprint, request
 
 api = Blueprint('api', __name__)
 
-@api.route('/endpoint')
-def api_endpoint():
-    return {"message": "Hello from API!"}
+# Create account route
+@api.route('/accounts', methods=['POST'])
+def create_account():
+    # Logic to create an account
+    return {"message": "Account created successfully"}
+
+# Read account route
+@api.route('/accounts/<account_id>', methods=['GET'])
+def read_account(account_id):
+    # Logic to read an account
+    return {"message": f"Reading account with ID: {account_id}"}
+
+# Delete account route
+@api.route('/accounts/<account_id>', methods=['DELETE'])
+def delete_account(account_id):
+    # Logic to delete an account
+    return {"message": f"Deleted account with ID: {account_id}"}
