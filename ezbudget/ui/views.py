13503946a471a5a323586b57d6928fb8cc7a1a57
Jonathan Davidson <jdavidson@netsplasher.com>
2023-11-29 08:39:29 -0600

    add login route to redirect to cognito

--- ezbudget/ui/views.py
+++ ezbudget/ui/views.py
@@ -1,5 +1,6 @@
 from ezbudget.services.auth_service import request_token
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, redirect, request, current_app
+from urllib.parse import urlencode
 
 ui = Blueprint('ui', __name__)
 
@@ -7,10 +8,22 @@ ui = Blueprint('ui', __name__)
 def home():
     return render_template('home.html')
 
+@ui.route('/login', methods=['GET'])
+def login():
+    domain = current_app.config.get('COGNITO_DOMAIN')
+    query_params = {
+        'client_id': current_app.config.get('COGNITO_CLIENT_ID'),
+        'response_type': 'code',
+        'redirect_uri': current_app.config.get('COGNITO_REDIRECT_URI')
+    }
+    query_string = urlencode(query_params)
+    authorize_url = "https://{}/oauth2/authorize?{}".format(domain, query_string)
+
+    return redirect(authorize_url)
+
 @ui.route('/auth/signin', methods=['GET'])
 def signin():
     code = request.args.get('code')
-    
     token = request_token(code);
     # Your code logic here
     return 'Signin GET request received with code: {}'.format(code)
