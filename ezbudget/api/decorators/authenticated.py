28aa51ef3a838d2b90f64d79a5d106e797cd8ccd
Jonathan Davidson <jdavidson@netsplasher.com>
2023-12-01 11:17:34 -0600

    Add token validation to auth_required decorator

--- ezbudget/api/decorators/authenticated.py
+++ ezbudget/api/decorators/authenticated.py
@@ -1,12 +1,26 @@
-from flask import request
+from flask import current_app, request
 from functools import wraps
+from jwt import decode, InvalidTokenError, PyJWKClient
 
 def auth_required(func):
     @wraps(func)
+
     def wrapper(*args, **kwargs):
-        if request.headers.get('Authorization') == 'Bearer 123':
-            return func(*args, **kwargs)
+        authHeader = request.headers.get('Authorization')
+        token = authHeader.split('Bearer ')[1] if authHeader.startswith('Bearer ') else None
+        jwk_url = current_app.config['COGNITO_JWK_URL']
+
+        if token:
+            try:
+                jwks_client = PyJWKClient(jwk_url)
+                signing_key = jwks_client.get_signing_key_from_jwt(token)
+                decoded_token = decode(token, signing_key.key, algorithms=['RS256'])
+                print(decoded_token)
+                return func(*args, **kwargs)
+            except InvalidTokenError:
+                return {"message": "Invalid token"}, 401
         else:
             return {"message": "Unauthorized"}, 401
+        
     return wrapper
   
